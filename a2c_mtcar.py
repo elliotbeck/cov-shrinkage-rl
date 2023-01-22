43f66e84cde319b583fa64b8524b5bd076970023
Elliot Beck <elliot.beck@hotmail.com>
2023-01-22 10:53:20 +0100

    remove typo

--- a2c_mtcar.py
+++ a2c_mtcar.py
@@ -167,8 +167,7 @@ def main():
         if np.mean(stats[-100:]) > 90 and len(stats) >= 100:
             print(np.mean(stats[-100:]))
             print("Solved successfully!")
-        else:
-            print("Not solved successfully...")
+
         
     # return the average score of the last 100 episodes
     return np.mean(stats[-100:])
