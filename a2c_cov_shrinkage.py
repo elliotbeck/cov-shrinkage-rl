20178cac34e490f49e1a41579c0461685b491be6
Elliot Beck <elliot.beck@hotmail.com>
2023-02-14 14:45:15 +0100

    add benchmark performance measure

--- a2c_cov_shrinkage.py
+++ a2c_cov_shrinkage.py
@@ -6,7 +6,7 @@ import numpy as np
 import hdf5storage
 import argparse
 from itertools import count
-from sklearn import preprocessing
+from sklearn.covariance import LedoitWolf
 
 import torch
 import torch.optim as optim
@@ -50,13 +50,15 @@ print("Using: ", device)
 # initialize lists
 last_score_plot = [0.]
 avg_score_plot = [0.]
+last_score_benchmark_plot = [0.]
+avg_score_benchmark_plot = [0.]
 
 # define arguments
 parser = argparse.ArgumentParser(description='PyTorch A2C for Covariance Shrinkage')
-parser.add_argument('--gamma', type=float, default=0.9)
-parser.add_argument('--actor_lr', type=float, default=1e-6)
-parser.add_argument('--critic_lr', type=float, default=5e-5)  
-parser.add_argument('--hidden_dim', type=int, default=200)
+parser.add_argument('--gamma', type=float, default=0.98)
+parser.add_argument('--actor_lr', type=float, default=1e-3)
+parser.add_argument('--critic_lr', type=float, default=5e-2)  
+parser.add_argument('--hidden_dim', type=int, default=100)
 parser.add_argument('--max_episode', type=int, default=1000)
 parser.add_argument('--seed', type=int, default=1)
 
@@ -68,8 +70,10 @@ env = portfolio_shrink_env(data, vintages)
 
 # set up actor and critic
 actor = Actor(hidden_dim = cfg.hidden_dim, 
+              input_dim = 5, # TODO: Make this dynamic
               output_dim = 1).to(device)
 critic = Critic(hidden_dim = cfg.hidden_dim,
+                input_dim = 5, # TODO: Make this dynamic
                 output_dim = 1).to(device)
 
 # set up optimizers
@@ -89,6 +93,7 @@ def main():
         
         # set up the score
         episode_score = 0
+        episode_score_benchmark = 0
         
         # iterate through the steps of the game
         for t in count():
@@ -106,22 +111,26 @@ def main():
                                   values='stock_return',
                                   index='date')
             
-            # get the covariance matrix
-            state_covariance = state_wide.cov()
-            state_covariance = preprocessing.scale(state_covariance)
-            state_covariance = pd.DataFrame(state_covariance)
-            
+            # get shrinkage intensities
+            state_shrinkage = [LedoitWolf().fit(state_wide.iloc[-252*i:, :]).shrinkage_ for i in range(1, 6)]
+            state_shrinkage = np.array(state_shrinkage)
+                        
             # get the action of the agent
-            action = get_action(state_covariance, actor, env, device)
+            action = get_action(state_shrinkage, actor, env, device)
             
-            # print action
-            print(str(t) + " " + str(action))
+            # # print action
+            # print(str(t) + " " + str(action-state_shrinkage[0]))
+            # print(str(t) + " " + str(action*10) + " " + str(state_shrinkage[0]*10))
             
             # get the next state, reward and status
             next_state, reward, done, _ = env.step(action) # type: ignore    
+            
+            # get the next state, reward and status for benchmark
+            reward_benchmark = -1*env.get_portfolio_volatility(state_shrinkage[0]) # type: ignore    
                     
             # update episode score
             episode_score += reward # type: ignore  
+            episode_score_benchmark += reward_benchmark # type: ignore
                       
             # replace permnos
             next_state = next_state.assign(permno=permnos_vector)
@@ -133,35 +142,37 @@ def main():
                                        index='date')
             
             # get the covariance matrix
-            next_state_covariance = next_state_wide.cov()
+            # get shrinkage intensities next state
+            next_state_shrinkage = [LedoitWolf().fit(next_state_wide.iloc[-252*i:, :]).shrinkage_ for i in range(1, 6)]
+            next_state_shrinkage = np.array(state_shrinkage)
 
             # render the game
             # env.render()
 
             # calculate the target
-            target = reward + cfg.gamma * get_state_value(next_state_covariance, critic, device)
+            target = reward + cfg.gamma * get_state_value(next_state_shrinkage, critic, device)
             
             # calculate the td error
-            td_error = target - get_state_value(state_covariance, critic, device)
+            td_error = target - get_state_value(state_shrinkage, critic, device)
             
             # only update the actor and critic every 10 steps to reduce variance
             # of gradient descent steps. Furthermore, only update the actor for the 
             # first 200 episodes
             # if t%2==0 and i_episode < 200: 
             # update actor (gradient descent) 
-            update_actor(state_covariance,
-                            actor, 
-                            action, 
-                            td_error,
-                            actor_optimizer, 
-                            device)
+            update_actor(state_shrinkage,
+                         actor, 
+                         action, 
+                         td_error,
+                         actor_optimizer, 
+                         device)
             
             # update critic (gradient descent) 
-            update_critic(state_covariance, 
-                            target, 
-                            critic, 
-                            critic_optimizer, 
-                            device)
+            update_critic(state_shrinkage, 
+                          target, 
+                          critic, 
+                          critic_optimizer, 
+                          device)
 
             if done:
                 
@@ -170,12 +181,19 @@ def main():
                     (avg_score_plot[-1] * len(avg_score_plot) + episode_score)/(len(avg_score_plot)+1)) # type: ignore                
                 # append last score to list
                 last_score_plot.append(float(episode_score))
-                
+                avg_score_benchmark_plot.append(
+                    (avg_score_benchmark_plot[-1] * len(avg_score_benchmark_plot) + episode_score_benchmark)/(len(avg_score_benchmark_plot)+1)) # type: ignore                
+                # append last score to list
+                last_score_benchmark_plot.append(float(episode_score_benchmark))
+                print(last_score_plot)
+                print(last_score_benchmark_plot)
                 # plot intermediate results
                 plt.title('reward')
                 plt.xlabel('episode')
-                plt.plot(last_score_plot, 'b-')
-                plt.plot(avg_score_plot, 'g-')
+                plt.plot(last_score_plot, 'c-', legend='agent')
+                plt.plot(avg_score_plot, 'g-', legend='agent average')
+                plt.plot(last_score_benchmark_plot, 'r-', legend='benchmark')
+                plt.plot(avg_score_benchmark_plot, 'y-', legend='benchmark average')
                 plt.draw()
                 plt.pause(0.001)
                 
